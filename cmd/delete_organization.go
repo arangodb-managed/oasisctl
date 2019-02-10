5a7e02983effd9ad3809a71064d71e4f61cddd38
Ewout Prangsma <ewout@prangsma.net>
2019-02-10 08:32:35 +0100

    Added delete organization & project commands

---
+++ cmd/delete_organization.go
@@ -0,0 +1,54 @@
+//
+// DISCLAIMER
+//
+// Copyright 2019 ArangoDB Inc, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	rm "github.com/arangodb-managed/apis/resourcemanager/v1"
+)
+
+var (
+	// deleteOrganizationCmd deletes an organization that the user has access to
+	deleteOrganizationCmd = &cobra.Command{
+		Use:   "organization",
+		Short: "Delete an organization the authenticated user has access to",
+		Run:   deleteOrganizationCmdRun,
+	}
+	deleteOrganizationArgs struct {
+		organizationID string
+		projectID      string
+	}
+)
+
+func init() {
+	deleteCmd.AddCommand(deleteOrganizationCmd)
+	f := deleteOrganizationCmd.Flags()
+	f.StringVarP(&deleteOrganizationArgs.organizationID, "organization-id", "o", defaultOrganization(), "Identifier of the organization")
+}
+
+func deleteOrganizationCmdRun(cmd *cobra.Command, args []string) {
+	// Connect
+	conn := mustDialAPI()
+	rmc := rm.NewResourceManagerServiceClient(conn)
+	ctx := contextWithToken()
+
+	// Fetch organization
+	item := mustSelectOrganization(ctx, deleteOrganizationArgs.organizationID, rmc)
+
+	// Delete project
+	if _, err := rmc.DeleteOrganization(ctx, item); err != nil {
+		cliLog.Fatal().Err(err).Msg("Failed to delete organization")
+	}
+
+	// Show result
+	fmt.Println("Deleted organization!")
+}
