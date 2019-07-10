f67e325bd51eab0c80e611a55ccf731fbb08ff3c
Robert Stam <git@stamware.com>
2019-07-10 10:08:11 +0200

    In 'get server limits' the providerID and organizationID are specified in the wrong order.

--- cmd/data/get_server_limits.go
+++ cmd/data/get_server_limits.go
@@ -60,7 +60,7 @@ func init() {
 				project := selection.MustSelectProject(ctx, log, projectID, cargs.organizationID, rmc)
 
 				// Selection region
-				region := selection.MustSelectRegion(ctx, log, regionID, project.OrganizationId, cargs.providerID, platformc)
+				region := selection.MustSelectRegion(ctx, log, regionID, cargs.providerID, project.OrganizationId, platformc)
 
 				// Fetch limits
 				item, err := datac.GetServersSpecLimits(ctx, &data.ServersSpecLimitsRequest{
