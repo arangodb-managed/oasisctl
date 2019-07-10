5e31b6db0e58d1dbbd3b0e15c354efd21a1f7166
Robert Stam <git@stamware.com>
2019-07-10 13:27:18 +0200

    Added support for servers-presets

---
+++ cmd/list_servers.go
@@ -0,0 +1,26 @@
+//
+// DISCLAIMER
+//
+// Copyright 2019 ArangoDB Inc, Cologne, Germany
+//
+// Author Robert Stam
+//
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	// ListServersCmd is based for other commands
+	ListServersCmd = &cobra.Command{
+		Use:   "servers",
+		Short: "List servers information",
+		Run:   ShowUsage,
+	}
+)
+
+func init() {
+	ListCmd.AddCommand(ListServersCmd)
+}
