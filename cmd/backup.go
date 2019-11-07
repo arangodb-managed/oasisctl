1c8dfa9f9f2035b5b115ec8ea0ead44f4390d142
Gergely Brautigam <gergely@arangodb.com>
2019-11-07 11:37:19 +0100

    Working download.

---
+++ cmd/backup.go
@@ -0,0 +1,26 @@
+//
+// DISCLAIMER
+//
+// Copyright 2019 ArangoDB Inc, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	// BackupCmd is root for various `backup ...` commands
+	BackupCmd = &cobra.Command{
+		Use:   "backup",
+		Short: "Backup commands",
+		Run:   ShowUsage,
+	}
+)
+
+func init() {
+	RootCmd.AddCommand(BackupCmd)
+}
