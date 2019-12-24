e713a49e6e77cbe0c9b4b84aefdbcc16dec539c5
Ewout Prangsma <ewout@prangsma.net>
2019-12-24 12:59:59 +0100

    OAS-739 | Added wait deployment command

---
+++ cmd/wait.go
@@ -0,0 +1,26 @@
+//
+// DISCLAIMER
+//
+// Copyright 2019 ArangoDB Inc, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	// WaitCmd is root for various `wait ...` commands
+	WaitCmd = &cobra.Command{
+		Use:   "wait",
+		Short: "Wait for a status change",
+		Run:   ShowUsage,
+	}
+)
+
+func init() {
+	RootCmd.AddCommand(WaitCmd)
+}
