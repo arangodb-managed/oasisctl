3304ff3842604faf7223d8b30ad833da6bcee44d
joerg84 <joerg.schad@gmail.com>
2020-01-06 16:10:33 +0100

    Added SelectOrganization function returning an explicit error.

--- pkg/selection/organization.go
+++ pkg/selection/organization.go
@@ -10,26 +10,38 @@ package selection
 
 import (
 	"context"
-
-	"github.com/rs/zerolog"
-
+	"errors"
+	"fmt"
 	common "github.com/arangodb-managed/apis/common/v1"
 	rm "github.com/arangodb-managed/apis/resourcemanager/v1"
+	"github.com/rs/zerolog"
 )
 
-// MustSelectOrganization fetches the organization with given ID.
+// MustSelectOrganization fetches the organization with given ID and fails if no organization is found.
 // If no ID is specified, all organizations are fetched and if the user
 // is member of exactly 1, that organization is returned.
 func MustSelectOrganization(ctx context.Context, log zerolog.Logger, id string, rmc rm.ResourceManagerServiceClient) *rm.Organization {
+
+	err, org := SelectOrganization(ctx, log, id, rmc)
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	return org
+}
+
+// SelectOrganization fetches the organization with given ID or returns an error if not found.
+// If no ID is specified, all organizations are fetched and if the user
+// is member of exactly 1, that organization is returned.
+func SelectOrganization(ctx context.Context, log zerolog.Logger, id string, rmc rm.ResourceManagerServiceClient) (error, *rm.Organization) {
 	if id == "" {
 		list, err := rmc.ListOrganizations(ctx, &common.ListOptions{})
 		if err != nil {
-			log.Fatal().Err(err).Msg("Failed to list organizations")
+			return errors.New("Failed to list organizations"), nil
 		}
 		if len(list.Items) != 1 {
-			log.Fatal().Err(err).Msgf("You're member of %d organizations. Please specify one explicitly.", len(list.Items))
+			return errors.New(fmt.Sprintf("You're member of %d organizations. Please specify one explicitly.", len(list.Items))), nil
 		}
-		return list.Items[0]
+		return nil, list.Items[0]
 	}
 	result, err := rmc.GetOrganization(ctx, &common.IDOptions{Id: id})
 	if err != nil {
@@ -39,12 +51,12 @@ func MustSelectOrganization(ctx context.Context, log zerolog.Logger, id string, 
 			if err == nil {
 				for _, x := range list.Items {
 					if x.GetName() == id || x.GetUrl() == id {
-						return x
+						return nil, x
 					}
 				}
 			}
 		}
-		log.Fatal().Err(err).Str("organization", id).Msg("Failed to get organization")
+		return errors.New(err.Error() + "; organization: Failed to get organization"), nil
 	}
-	return result
+	return nil, result
 }
