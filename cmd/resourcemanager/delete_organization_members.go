343bd4e41b902639a5bf92421263673be6f7f049
Gergely Brautigam <gergely@arangodb.com>
2020-01-16 15:45:48 +0100

    Delete organization member

--- cmd/resourcemanager/delete_organization_members.go
+++ cmd/resourcemanager/delete_organization_members.go
@@ -11,33 +11,35 @@ package rm
 import (
 	"fmt"
 
-	common "github.com/arangodb-managed/apis/common/v1"
-	rm "github.com/arangodb-managed/apis/resourcemanager/v1"
-
 	"github.com/spf13/cobra"
 
+	common "github.com/arangodb-managed/apis/common/v1"
+	iam "github.com/arangodb-managed/apis/iam/v1"
+	rm "github.com/arangodb-managed/apis/resourcemanager/v1"
+
 	"github.com/arangodb-managed/oasisctl/cmd"
+	"github.com/arangodb-managed/oasisctl/pkg/selection"
 )
 
 var (
 	// deleteOrgMembersCmd deletes a list of members from an organization
 	deleteOrgMembersCmd = &cobra.Command{
-		Use:   "group",
-		Short: "Add members to group",
+		Use:   "members",
+		Short: "Delete members from organization",
 		Run:   deleteGroupMembersCmdRun,
 	}
 	deleteOrgMembersArgs struct {
 		organizationID string
-		userIDs        []string
+		userEmails     []string
 	}
 )
 
 func init() {
-	cmd.DeleteCmd.AddCommand(deleteOrgMembersCmd)
+	deleteOrganizationCmd.AddCommand(deleteOrgMembersCmd)
 
 	f := deleteOrgMembersCmd.Flags()
 	f.StringVarP(&deleteOrgMembersArgs.organizationID, "organization-id", "o", cmd.DefaultOrganization(), "Identifier of the organization")
-	deleteOrgMembersArgs.userIDs = *f.StringSliceP("user-ids", "u", []string{}, "A coma separated list of user ids")
+	deleteOrgMembersArgs.userEmails = *f.StringSliceP("user-emails", "u", []string{}, "A comma separated list of user email addresses")
 
 }
 
@@ -50,6 +52,7 @@ func deleteGroupMembersCmdRun(c *cobra.Command, args []string) {
 
 	// Connect
 	conn := cmd.MustDialAPI()
+	iamc := iam.NewIAMServiceClient(conn)
 	rmc := rm.NewResourceManagerServiceClient(conn)
 	ctx := cmd.ContextWithToken()
 
@@ -60,24 +63,31 @@ func deleteGroupMembersCmdRun(c *cobra.Command, args []string) {
 	if org.IsDeleted {
 		log.Fatal().Str("organization_id", organizationID).Msg("May not delete members from deleted organization.")
 	}
-	var members *rm.MemberList
-	// Create Member list
-	for _, u := range cargs.userIDs {
-		if ok, err := rmc.IsMemberOfOrganization(ctx, &rm.IsMemberOfOrganizationRequest{UserId: u, OrganizationId: organizationID}); err != nil {
-			log.Fatal().Err(err).Msg("Can't determine if user is part of the organization or not.")
-		} else if !ok.Member {
-			log.Fatal().Str("user-id", u).Str("organization-id", organizationID).Msg("User is not a member of the organization.")
-		}
-		member := &rm.Member{}
-		member.UserId = u
-	}
-	// Delete members
-	_, err = rmc.DeleteOrganizationMembers(ctx, &rm.OrganizationMembersRequest{
-		OrganizationId: organizationID,
-		Members:        members,
-	})
+
+	membersToDelete := &rm.MemberList{Items: make([]*rm.Member, 0)}
+	members, err := rmc.ListOrganizationMembers(ctx, &common.ListOptions{ContextId: organizationID})
 	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to list organization members.")
+	}
+	emailIDMap, err := selection.GenerateUserEmailMap(ctx, members, iamc)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to find user.")
+	}
+
+	for _, e := range cargs.userEmails {
+		if id, ok := emailIDMap[e]; !ok {
+			log.Fatal().Str("email", e).Str("organization-id", organizationID).Msg("User is not a member of the organization.")
+		} else {
+			membersToDelete.Items = append(membersToDelete.Items, &rm.Member{UserId: id})
+		}
+	}
+
+	if _, err = rmc.DeleteOrganizationMembers(ctx, &rm.OrganizationMembersRequest{
+		OrganizationId: organizationID,
+		Members:        membersToDelete,
+	}); err != nil {
 		log.Fatal().Err(err).Msg("Failed to delete users.")
 	}
+
 	fmt.Println("Success!")
 }
