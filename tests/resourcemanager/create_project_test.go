be7eb12997e7f6ea89a9256538e82406804e435f
Gergely Brautigam <gergely@arangodb.com>
2020-09-09 13:31:54 +0200

    Added some coverage for projects

---
+++ tests/resourcemanager/create_project_test.go
@@ -0,0 +1,85 @@
+//
+// DISCLAIMER
+//
+// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+// Author Gergely Brautigam
+//
+// +build e2e
+
+package resourcemanager
+
+import (
+	"testing"
+
+	"github.com/arangodb-managed/oasisctl/tests"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	common "github.com/arangodb-managed/apis/common/v1"
+	rm "github.com/arangodb-managed/apis/resourcemanager/v1"
+
+	"github.com/arangodb-managed/oasisctl/cmd"
+)
+
+func TestCreateProject(t *testing.T) {
+	cmd.RootCmd.PersistentPreRun(nil, nil)
+	ctx := cmd.ContextWithToken()
+	conn := cmd.MustDialAPI()
+	rmc := rm.NewResourceManagerServiceClient(conn)
+	org := cmd.DefaultOrganization()
+
+	if org == "" {
+		// Get the first organization if default is not set.
+		list, err := rmc.ListOrganizations(ctx, &common.ListOptions{})
+		assert.NoError(t, err)
+		require.NotEmpty(t, list.GetItems())
+		org = list.GetItems()[0].GetId()
+	}
+
+	testProj := "testCreateProject"
+	defer func() {
+		list, err := rmc.ListProjects(ctx, &common.ListOptions{ContextId: org})
+		if err != nil {
+			t.Log(err)
+		}
+
+		// We only delete the test organization
+		for _, project := range list.GetItems() {
+			if project.GetName() == testProj {
+				if _, err := rmc.DeleteProject(ctx, &common.IDOptions{Id: project.GetId()}); err != nil {
+					t.Log(err)
+				}
+				break
+			}
+		}
+	}()
+
+	compare := `^Success!
+Id          \d+
+Name        ` + testProj + `
+Description 
+Url         /Organization/\d+/Project/.*
+Created-At  .*
+Deleted-At  -
+$`
+
+	args := []string{"create", "project", "--name=" + testProj}
+	out, err := tests.RunCommand(args)
+	require.NoError(t, err)
+	assert.True(t, tests.CompareOutput(out, []byte(compare)))
+}
---
+++ tests/resourcemanager/get_project_test.go
@@ -0,0 +1,71 @@
+//
+// DISCLAIMER
+//
+// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+// Author Gergely Brautigam
+//
+// +build e2e
+
+package resourcemanager
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	common "github.com/arangodb-managed/apis/common/v1"
+	rm "github.com/arangodb-managed/apis/resourcemanager/v1"
+
+	"github.com/arangodb-managed/oasisctl/cmd"
+	"github.com/arangodb-managed/oasisctl/tests"
+)
+
+func TestGetProject(t *testing.T) {
+	cmd.RootCmd.PersistentPreRun(nil, nil)
+	ctx := cmd.ContextWithToken()
+	conn := cmd.MustDialAPI()
+	org := cmd.DefaultOrganization()
+	rmc := rm.NewResourceManagerServiceClient(conn)
+
+	if org == "" {
+		// Get the first organization if default is not set.
+		list, err := rmc.ListOrganizations(ctx, &common.ListOptions{})
+		assert.NoError(t, err)
+		require.NotEmpty(t, list.GetItems())
+		org = list.GetItems()[0].GetId()
+	}
+	project, err := rmc.CreateProject(ctx, &rm.Project{Name: "testProject", OrganizationId: org})
+	assert.NoError(t, err)
+	defer func() {
+		if _, err := rmc.DeleteProject(ctx, &common.IDOptions{Id: project.GetId()}); err != nil {
+			t.Log(err)
+		}
+	}()
+	compare := `^Id          \d+
+Name        ` + project.GetName() + `
+Description 
+Url         /Organization/\d+/Project/.*
+Created-At  .*
+Deleted-At  -
+$`
+	args := []string{"get", "project", "--project-id=" + project.GetId()}
+	out, err := tests.RunCommand(args)
+	require.NoError(t, err)
+	assert.True(t, tests.CompareOutput(out, []byte(compare)))
+}
