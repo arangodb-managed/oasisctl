c1fce6e9ab01ae920f526ae67429b1ea9e003eb1
Adonis Murati <adonis.murati@arangodb.com>
2023-06-08 14:51:30 +0200

    OAS-7428 | ensure CIDR range added to IP allowlist works during update

--- cmd/security/update_ipallowlist.go
+++ cmd/security/update_ipallowlist.go
@@ -24,7 +24,6 @@ package security
 
 import (
 	"fmt"
-	"sort"
 
 	"github.com/spf13/cobra"
 	flag "github.com/spf13/pflag"
@@ -91,9 +90,12 @@ func init() {
 					hasChanges = true
 				}
 				cidrRanges := make(map[string]struct{})
-				for _, x := range item.GetCidrRanges() {
+				existingCidrRanges := item.GetCidrRanges()
+
+				for _, x := range existingCidrRanges {
 					cidrRanges[x] = struct{}{}
 				}
+
 				if len(cargs.addCidrRanges) > 0 {
 					for _, x := range cargs.addCidrRanges {
 						if _, found := cidrRanges[x]; !found {
@@ -117,12 +119,31 @@ func init() {
 				if !hasChanges {
 					fmt.Println("No changes")
 				} else {
-					// Rebuild CidrRanges list
-					item.CidrRanges = make([]string, 0, len(cidrRanges))
-					sort.Strings(item.CidrRanges)
-					for x := range cidrRanges {
-						item.CidrRanges = append(item.CidrRanges, x)
+					// Rebuild CIDR ranges list
+					updatedCidrRanges := make([]string, 0, len(existingCidrRanges))
+					for _, x := range existingCidrRanges {
+						if _, found := cidrRanges[x]; found {
+							updatedCidrRanges = append(updatedCidrRanges, x)
+						}
 					}
+
+					if len(cargs.addCidrRanges) > 0 {
+						item.CidrRanges = append(updatedCidrRanges, cargs.addCidrRanges...)
+					} else {
+						item.CidrRanges = updatedCidrRanges
+					}
+
+					if len(cargs.removeCidrRanges) > 0 {
+						updatedCidrRanges = make([]string, 0, len(item.CidrRanges))
+						for _, x := range item.CidrRanges {
+							if _, found := cidrRanges[x]; found {
+								updatedCidrRanges = append(updatedCidrRanges, x)
+							}
+						}
+
+						item.CidrRanges = updatedCidrRanges
+					}
+
 					// Update IP allowlist
 					updated, err := securityc.UpdateIPAllowlist(ctx, item)
 					if err != nil {
